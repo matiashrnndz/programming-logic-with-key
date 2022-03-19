d7843f757799d9923f630637f5f40b4575a7e5bd
Matias Hernandez <matias.hernandez@pedidosya.com>
2022-03-19 00:19:57 -0300

    Refactored LinearSearch

--- key/src/main/java/com/key/LinearSearch.java
+++ key/src/main/java/com/key/LinearSearch.java
@@ -3,15 +3,14 @@ package com.key;
 public class LinearSearch {
   
   /*@ public normal_behavior
-    @ ensures \result >= 0 ==>
-    @           \result < arr.length && arr[\result] == key;
-    @ ensures \result < 0 ==>
-    @           (\forall  int k;
-    @                     0 <= k && k < arr.length;
-    @                     arr[k] != key);
+    @ ensures (0 <= \result && \result < arr.length && arr[\result] == key) 
+    @      || (\result == -1 && !(\exists int k;
+    @                                    0 <= k && k < arr.length;
+    @                                    arr[k] == key));
     @ assignable \strictly_nothing;
     @*/
   public int linearSearch (int[] arr, int key) {
+    int i = 0;
     /*@ loop_invariant 0 <= i && i <= arr.length;
       @ loop_invariant (\forall int k;
       @                         0 <= k && k < i;
@@ -19,9 +18,10 @@ public class LinearSearch {
       @ assignable \strictly_nothing;
       @ decreases arr.length - i;
       @*/
-    for (int i = 0; i < arr.length; i++) {
-      if (arr[i] == key) return i;
+    while (i != arr.length && arr[i] != key) {
+      i = i + 1;
     }
-    return -1;
+
+    return i == arr.length ? -1 : i;
   }
 }
